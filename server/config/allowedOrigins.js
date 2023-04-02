87ab7a29679de9fb44110d9fadfdf248c184f91a
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2023-04-03 03:25:18 +0530

    update cors config

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = [
+  'http://localhost:3000',
+  process.env.FRONTEND_URL,
+  process.env.ALT_FRONTEND_URL,
+];
 
 module.exports = allowedOrigins;
