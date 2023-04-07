6be73a84a9e042383d99e91bd7d0e81b7e898fc8
tharindu432 <tharinduchaturanga432@gmail.com>
2023-04-07 11:49:57 +0530

    final commit

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -113,7 +113,7 @@ const Landing = ({ contact, about }) => {
       </section>
       <section className="guidlines text-primary" id="guidelines">
         <div>
-          <h2 className="hidden top">guidlines</h2>
+          <h2 className="hidden top">guidelines</h2>
           <ul className="hidden left">
             <li>
               All the participants must be undergraduates and should be from the
@@ -141,7 +141,7 @@ const Landing = ({ contact, about }) => {
           </ul>
         </div>
         <h1 className="vertical-header text-border-gradient hidden top">
-          guidlines
+          guidelines
         </h1>
       </section>
       <section className="timeline" id="timeline">
